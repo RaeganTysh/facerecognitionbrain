8a855c31d104b571dfeff6bec1efe0a6c96dd59f
RaeganTysh <rae-gan@shaw.ca>
2021-03-05 11:40:39 -0700

    initail commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'; 
+import Navigation from './components/navigation/navigation';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation/>
+        
+        {/*<Logo />
+          <ImageLinkForm />
+          <FacRecongnition />*/}
+      
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//simple comonent with no state so just need a simple function
+
+const Navigation = () => {
+    return (
+        <nav>
+            <p>Sign Out</p>
+
+        </nav>
+    );
+}
+export default Navigation;
