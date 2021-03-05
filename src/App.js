ed32c5b5cb49fa45467a1b50d6d9e104bd656d66
RaeganTysh <rae-gan@shaw.ca>
2021-03-05 15:03:39 -0700

    imagelinkform component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'; 
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
+import ImageLinkForm from './components/imageLinkForm/imagelinkform';
 import './App.css';
 
 
@@ -10,8 +11,8 @@ class App extends Component {
       <div className="App">
         <Navigation/>        
         <Logo />
-        {/*<ImageLinkForm />
-          
+        <ImageLinkForm />
+        {/* 
           <FacRecongnition />*/}
       
     </div>
---
+++ src/components/imageLinkForm/imagelinkform.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './imagelinkform.css';
+
+
+//simple copmonent with no state so just need a simple function
+
+const ImageLinkForm = () => {
+    return (
+        <div >
+            <p className='f3'>
+                {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+            </p>
+            <div className="center">
+            <div className= " form center pa4 br3 shadow-5" >
+                <input className='f4 pa2 w-80 center' type='text' />
+                <button className= 'ma1 w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+            </div>
+            </div>
+            
+
+        </div >
+    );
+}
+export default ImageLinkForm;
+
+//Tachyons classNames
+//f size 4
+//pa padding
+//w 70% 
+//ma margin
+//br border radius
