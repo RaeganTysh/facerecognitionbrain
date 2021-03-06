35325e24457abb7562b9e603a444f55ebf2e6d1f
RaeganTysh <rae-gan@shaw.ca>
2021-03-05 21:05:44 -0700

    particles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,128 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imagelinkform';
+import Rank from './components/rank/rank';
 import './App.css';
+import Particles from "react-tsparticles";
+
+const particleOptions = {
+  /*background: {
+    color: {
+      value: "#0d47a1",
+    },
+  },*/
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 100,
+        duration: 2,
+        opacity: 0.2,
+        size: 40,
+      },
+      push: {
+        quantity: 2,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 4,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 500,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.6,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 1,
+    },
+  },
+  detectRetina: true,
+}
 
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.particlesInit = this.particlesInit.bind(this);
+    this.particlesLoaded = this.particlesLoaded.bind(this);
+  }
+
+  particlesInit(main) {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  }
+
+  particlesLoaded(container) {
+    console.log(container);
+  }
   render() {
     return (
       <div className="App">
-        <Navigation/>        
+        <Navigation />
         <Logo />
+        <Particles 
+          className='particles'
+          id="tsparticles"
+          init={this.particlesInit}
+          loaded={this.particlesLoaded}
+          params = {particleOptions}
+        />
+
+        <Rank />
         <ImageLinkForm />
+
         {/* 
           <FacRecongnition />*/}
-      
-    </div>
+
+      </div>
     );
   }
 }
---
+++ src/components/rank/rank.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+//simple copmonent with no state so just need a simple function
+
+const Rank = () => {
+    return (
+        <div >
+            <div className='white f3'>
+                {'Raegan your current rank is....'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+
+
+
+        </div >
+    );
+}
+export default Rank;
