131f943343a09bc9417247cb2c8cecbcb1c0f610
RaeganTysh <rae-gan@shaw.ca>
2021-03-07 19:55:16 -0700

    Clarifai API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Particles from "react-tsparticles";
+import Clarifai from 'clarifai';
 import Navigation from './components/navigation/navigation';
 import Logo from './components/logo/logo';
 import ImageLinkForm from './components/imageLinkForm/imagelinkform';
 import Rank from './components/rank/rank';
 import './App.css';
-import Particles from "react-tsparticles";
 
-const app =new Clarifai.App({
-  apiKey:'9e5edc8105a84b0d9758712497600bfe'
+
+
+const app = new Clarifai.App({
+  apiKey: '9e5edc8105a84b0d9758712497600bfe'
 });
 
 const particleOptions = {
@@ -119,30 +122,64 @@ class App extends Component {
 
   onButtonSubmit = () => {
     console.log('click');
+    app.models
+        .predict(
+        // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+        // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+        // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+        // If that isn't working, then that means you will have to wait until their servers are back up. Another solution
+        // is to use a different version of their model that works like: `c0c0ac362b03416da06ab3fa36fb58e3`
+        // so you would change from:
+        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+        // to:
+        //.predict('c0c0ac362b03416da06ab3fa36fb58e3', this.state.input)
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
   }
 
 
-render() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Particles
-        className='particles'
-        id="tsparticles"
-        init={this.particlesInit}
-        loaded={this.particlesLoaded}
-        params={particleOptions}
-      />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={this.onInputChange}
-        onButtonSubmit={this.onButtonSubmit} />
-      {/*<FaceRecongnition />*/}
 
-    </div>
-  );
-}
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Particles
+          className='particles'
+          id="tsparticles"
+          init={this.particlesInit}
+          loaded={this.particlesLoaded}
+          params={particleOptions}
+        />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        {/*<FaceRecongnition />*/}
+
+      </div>
+    );
+  }
 }
 
 export default App;
