b084ca764d932b0c619d0e03bbcb9b57f0a37b17
RaeganTysh <rae-gan@shaw.ca>
2021-03-10 13:34:16 -0700

    facerecognition.js styling

--- src/components/facerecognition/facerecognition.js
+++ src/components/facerecognition/facerecognition.js
@@ -4,10 +4,16 @@ import React from 'react';
 
 const FaceRecognition = ({imageUrl}) => {
     return (
-        <div className='center'>
-            <img alt= "" src={imageUrl} />
+        <div className='center ma'>  
+            <div className= 'absolute mt2'>
+            <img alt= "" src={imageUrl} width='500px' height="auto" />
+            </div>
 
         </div>
     );
 }
 export default FaceRecognition;
+
+//tachyons styling
+//ma 
+//mt margin top
