ed0a3484e7de3a70c9f44e44129d96da9d920520
RaeganTysh <rae-gan@shaw.ca>
2021-03-13 10:37:14 -0700

    signin

---
+++ src/components/signin/signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+//simple comonent with no state so just need a simple function
+
+const SignIn =  {
+    return (
+        
+    );
+}
+export default FaceRecognition;
